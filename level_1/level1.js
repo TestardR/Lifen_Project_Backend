55d38c43dd881ff2c1559169dbdbe32923af9bad
unknown <rmtestard@gmail.com>
2019-03-08 15:00:51 +0100

    level 1 done

---
+++ level_1/level1.js
@@ -0,0 +1,85 @@
+/*
+We are building an online night-shift manager. Let's call it Planning de Garde :)
+Here is our plan:
+
+    a hospital schedule shifts for all their plannings
+    shift workers manage their shifts
+
+Level 1
+Once the hospital has scheduled all shifts, it want to know how much each shift worker is supposed to be paid. Each shift is paid according to the worker assigned to the shift.
+Write code that generates output.json from data.json
+*/
+
+// import data.json
+const data = require('./data.json');
+
+// Constructor
+function ShiftWorker(first_name, price_per_shift, id) {
+  this.first_name = first_name;
+  this.price_per_shift = price_per_shift;
+  this.id = id;
+
+  this.salary = function() {
+    let count = 0;
+    let salary = 0;
+    for (let i = 0; i < data.shifts.length; i++) {
+      if (data.shifts[i].user_id === this.id) {
+        count++;
+      }
+      salary = count * this.price_per_shift;
+    }
+    return salary;
+  };
+
+  this.payment = function() {
+    return `For this schedule, ${
+      this.first_name
+    } should be paid ${this.salary()} euros.`;
+  };
+
+  // List that will be parsed to JSON and written to output.json
+  this.addToList = function() {
+    list.push(this.payment());
+  };
+}
+
+// Constructor instanciation for Julie
+const Julie = new ShiftWorker(
+  data.workers[0].first_name,
+  data.workers[0].price_per_shift,
+  data.workers[0].id
+);
+
+// Constructor instanciation for Marc
+const Marc = new ShiftWorker(
+  data.workers[1].first_name,
+  data.workers[1].price_per_shift,
+  data.workers[1].id
+);
+
+// Constructor instanciation for Antoine
+const Antoine = new ShiftWorker(
+  data.workers[2].first_name,
+  data.workers[2].price_per_shift,
+  data.workers[2].id
+);
+
+// Write to output.json
+const list = [];
+Marc.addToList();
+Julie.addToList();
+Antoine.addToList();
+
+const obj = {
+  list
+};
+
+const addToOuput = function() {
+  const fs = require('fs');
+  fs.writeFile('output.json', JSON.stringify(obj), function(err) {
+    if (err) throw err;
+    console.log('complete');
+  });
+};
+
+addToOuput();
