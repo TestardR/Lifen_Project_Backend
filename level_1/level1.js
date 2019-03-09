3d4796b67578371a0a425747ba2a11ab2bad8937
unknown <rmtestard@gmail.com>
2019-03-09 16:35:57 +0100

    refactoring

--- level_1/level1.js
+++ level_1/level1.js
@@ -1,17 +1,6 @@
-/*
-We are building an online night-shift manager. Let's call it Planning de Garde :)
-Here is our plan:
-
-    a hospital schedule shifts for all their plannings
-    shift workers manage their shifts
-
-Level 1
-Once the hospital has scheduled all shifts, it want to know how much each shift worker is supposed to be paid. Each shift is paid according to the worker assigned to the shift.
-Write code that generates output.json from data.json
-*/
-
 // import data.json
 const data = require('./data.json');
+const fs = require('fs');
 
 // Constructor
 function ShiftWorker(first_name, price_per_shift, id) {
@@ -36,50 +25,31 @@ function ShiftWorker(first_name, price_per_shift, id) {
       this.first_name
     } should be paid ${this.salary()} euros.`;
   };
-
-  // List that will be parsed to JSON and written to output.json
-  this.addToList = () => {
-    list.push(this.payment());
-  };
 }
 
-// Constructor instanciation for Julie
-const Julie = new ShiftWorker(
-  data.workers[0].first_name,
-  data.workers[0].price_per_shift,
-  data.workers[0].id
-);
-
-// Constructor instanciation for Marc
-const Marc = new ShiftWorker(
-  data.workers[1].first_name,
-  data.workers[1].price_per_shift,
-  data.workers[1].id
-);
-
-// Constructor instanciation for Antoine
-const Antoine = new ShiftWorker(
-  data.workers[2].first_name,
-  data.workers[2].price_per_shift,
-  data.workers[2].id
-);
+const employees = [];
+for (let i = 0; i < data.workers.length; i++) {
+  employees.push(
+    new ShiftWorker(
+      data.workers[i].first_name,
+      data.workers[i].price_per_shift,
+      data.workers[i].id
+    )
+  );
+}
 
 // Write to output.json
 const list = [];
-Marc.addToList();
-Julie.addToList();
-Antoine.addToList();
 
-const obj = {
-  list
-};
+for (let employee of employees) {
+  list.push(employee.payment());
+}
 
-const addToOuput = () => {
-  const fs = require('fs');
-  fs.writeFile('output.json', JSON.stringify(obj), err => {
+const writeToOutput = () => {
+  fs.writeFile('output.json', JSON.stringify(list), err => {
     if (err) throw err;
     console.log('complete');
   });
 };
 
-addToOuput();
+writeToOutput();
