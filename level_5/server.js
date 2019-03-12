e23e93edfaa29d09b266411aad4ca7fc57fad58d
unknown <rmtestard@gmail.com>
2019-03-12 11:02:47 +0100

    level 5 deployment ready code

--- level_5/server.js
+++ level_5/server.js
@@ -32,6 +32,6 @@ mongoose
 app.use('/api/workers', workers);
 app.use('/api/shifts', shifts);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
