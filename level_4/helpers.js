66b3937b790bc04876cbc75770bf6ce0335b1e5a
unknown <rmtestard@gmail.com>
2019-03-12 09:41:28 +0100

    FirstOrDefault

--- level_4/helpers.js
+++ level_4/helpers.js
@@ -1,5 +1,6 @@
 module.exports = {
   // méthode de Linq en C#
+  // return first item with desired value || null
   FirstOrDefault: function(items, propertyName, searchValue) {
     for (var i = 0; i < items.length; i++) {
       if (items[i][propertyName] === searchValue) return items[i];
